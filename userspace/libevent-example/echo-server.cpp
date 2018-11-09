d1d30cce4467854cce68522471cc37b7629970e5
Herbert <wang_xiaoq@126.com>
2018-11-09 01:01:40 -0800

    enhence echo-server.cpp
    Signed-off-by: Herbert <wang_xiaoq@126.com>

--- userspace/libevent-example/echo-server.cpp
+++ userspace/libevent-example/echo-server.cpp
@@ -5,10 +5,15 @@
 #include <iostream>
 #include <strings.h>
 #include <string.h>
+#include <vector>
 
 #define PORT 34567
 #define BUFSIZE 4096
 #define BACKLOG 5
+#define MAXCONN 65535
+
+std::vector<std::shared_ptr<event>> read_events;
+std::vector<std::shared_ptr<event>> write_events;
 
 int start_tcp_server()
 {
@@ -43,14 +48,22 @@ void on_read(evutil_socket_t fd, short events, void *arg)
     int ret = recv(fd, buf, BUFSIZE - 1, 0);
     if (ret < 0) {
         return ;
+    } else if (ret == 0) {
+        event_del(read_events[fd].get());
+        event_del(write_events[fd].get());
     }
 
-    ret = send(fd, buf, strlen(buf), 0);
-    if (ret < 0) {
-        return ;
-    }
+    event_add(write_events[fd].get(), NULL);
 
     std::cout << "on_read end" <<std::endl;
+}
+
+void on_write(evutil_socket_t fd, short events, void *arg)
+{
+    const char *buf = "hello\n";
+    send(fd, buf, strlen(buf), 0);
+    event_del(write_events[fd].get());
+    std::cout << "on_write end" << std::endl;
 }
 
 void on_accept(evutil_socket_t fd, short events, void *arg)
@@ -60,14 +73,18 @@ void on_accept(evutil_socket_t fd, short events, void *arg)
     bzero(&saddr, sizeof(saddr));
     socklen_t len = sizeof(saddr);
     cfd = accept(fd, reinterpret_cast<struct sockaddr*>(&saddr), &len);
-    if (cfd < 0) {
+    if (cfd < 0 || cfd > MAXCONN) {
         return ;
     }
     evutil_make_socket_nonblocking(cfd);
 
     auto ev_base_ptr = *reinterpret_cast<std::shared_ptr<event_base>*>(arg);
-    event *ev = event_new(ev_base_ptr.get(), cfd, EV_READ|EV_PERSIST, on_read, NULL);
-    event_add(ev, NULL);
+    event *rev = event_new(ev_base_ptr.get(), cfd, EV_READ|EV_PERSIST, on_read, NULL);
+    event_add(rev, NULL);
+    read_events[cfd].reset(rev, event_free);
+
+    event *wev = event_new(ev_base_ptr.get(), cfd, EV_WRITE|EV_PERSIST, on_write, NULL);
+    write_events[cfd].reset(wev, event_free);
 
     std::cout << "on_accept end" << std::endl;
 
@@ -76,15 +93,21 @@ void on_accept(evutil_socket_t fd, short events, void *arg)
 
 int main(int argc, char *argv[])
 {
+    read_events.resize(MAXCONN);
+    write_events.resize(MAXCONN);
     std::shared_ptr<event_base> ev_base_ptr(event_base_new(), event_base_free);
     int fd = start_tcp_server();
-    if (fd < 0) {
+    if (fd < 0 || fd > MAXCONN) {
         return -1;
     }
     evutil_make_socket_nonblocking(fd);
 
     std::shared_ptr<event> ev_ptr(event_new(ev_base_ptr.get(), fd, EV_READ|EV_PERSIST, on_accept, reinterpret_cast<void *>(&ev_base_ptr)), event_free);
     event_add(ev_ptr.get(), NULL);
+
+    read_events[fd] = std::move(ev_ptr);
+    ev_ptr = nullptr;
+
     event_base_dispatch(ev_base_ptr.get());
 
     return 0;
